c12645b231108ba50af6303461886c3f79da3aed
thayline07 <thaylinesimioni@gmail.com>
2026-01-09 07:26:23 -0300

    .

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
-import BlogList from "@/templates/blog/blog-list";
+import { Metadata } from "next";
 import { allPosts } from "contentlayer/generated";
+import BlogList from "@/templates/blog/blog-list";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Dicas e estratégias para impulsionar seus negócios.",
+  robots: "index, follow",
+  openGraph: {
+    title: "Blog",
+    description: "Dicas e estratégias para impulsionar seus negócios.",
+    url: "https://site-set-landing-page-blog-3uu1.vercel.app/",
+    siteName: "Site.Set",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://site-set-landing-page-blog-3uu1.vercel.app/og-image.jpg",
+        width: 880,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ],
+  },
+};
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort(
