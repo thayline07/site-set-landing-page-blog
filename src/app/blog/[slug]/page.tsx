d3fa411661c65a138be37d5fed669e334c62cff1
thayline07 <thaylinesimioni@gmail.com>
2026-01-09 07:34:33 -0300

    .

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,34 @@
 import { notFound } from "next/navigation";
 import PostPage from "@/templates/blog/post-page";
 import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
 
 type BlogPostPageProps = {
   params: Promise<{
     slug: string;
   }>;
 };
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    robots: "index, follow",
+    authors: [{ name: post.author.name }],
+    openGraph: {
+      images: [post.image],
+    },
+  };
+}
 
 export const revalidate = 60;
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,23 @@ import "@/styles/globals.css";
 export const metadata: Metadata = {
   title: "Site.Set",
   description: "Venda seus produtos como afiliado em um único lugar.",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.Set",
+    description: "Venda seus produtos como afiliado em um único lugar.",
+    url: "https://site-set-landing-page-blog-3uu1.vercel.app/",
+    siteName: "Site.Set",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://site-set-landing-page-blog-3uu1.vercel.app/og-image.jpg",
+        width: 880,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
